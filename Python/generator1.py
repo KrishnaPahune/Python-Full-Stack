d9efebf23285533c8648228a6ae5a31899442ec9
Krishna Pahune <krishnapahune4@gmail.com>
2026-02-13 17:30:29 +0530

    Generators new code

--- Python/generator1.py
+++ Python/generator1.py
@@ -1,8 +1,15 @@
-def square_numbers(n):
+import sys
+
+square_list = []
+
+for i in range(1000000):
+    square_list.append(i**2)
+
+def generate_squares(n):
     for i in range(n):
-        yield i ** 2
+        yield i**2
+    
+generator = generate_squares(1000000)
 
-gen = square_numbers(5)
-
-for num in gen:
-    print(num)  # Output: 0, 1, 4, 9, 16
+print("Memory used by list:", sys.getsizeof(square_list), "bytes")
+print("Memory used by generator:", sys.getsizeof(generator), "bytes")
